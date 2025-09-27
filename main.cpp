cfbd82084a5b317258434f610b74ac34a6161e0f
alat414 <gustavo.alatriste@g.austincc.edu>
2025-09-27 14:31:36 -0500

    removing doctest header file from driver file

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@
 
 // https://github.com/doctest/doctest/blob/master/doc/markdown/tutorial.md
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
-#include "doctest.h"
+//#include "doctest.h"
 
 using namespace std;
 
